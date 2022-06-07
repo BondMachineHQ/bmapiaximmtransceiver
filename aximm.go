6141e09f89ad57c384a251fb612f06eeb3f515e0
Mirko Mariotti <mirko@mirkomariotti.it>
2022-06-07 17:52:39 +0200

    Read in a goroutine

--- aximm.go
+++ aximm.go
@@ -2,6 +2,7 @@ package bmapiaximmtransceiver
 
 import (
 	"context"
+	//	"fmt"
 	"log"
 	"os"
 	"sync"
@@ -25,6 +26,7 @@ func AXImmTransceiver(ctx context.Context, device string, debug bool) (chan<- ui
 	dst := make(chan uint8)
 	ended := make(chan struct{})
 	buff := make([]byte, 100)
+	rch := make(chan int)
 	wg.Add(1)
 	go func() {
 		if debug {
@@ -33,20 +35,35 @@ func AXImmTransceiver(ctx context.Context, device string, debug bool) (chan<- ui
 		defer port.Close()
 		defer wg.Done()
 		for {
-			n, err := port.Read(buff)
-			if err != nil {
-				log.Println(err)
-				continue
-			}
 
-			for i := 0; i < n; i++ {
-				select {
-				case <-ctx.Done():
-					if debug {
-						log.Println("transceiver: receiver exiting")
-					}
+			go func() {
+				n, err := port.Read(buff)
+				if err != nil {
+					log.Println(err)
+					rch <- 0
 					return
-				case dst <- buff[i]:
+				}
+				rch <- n
+				return
+			}()
+
+			select {
+			case <-ctx.Done():
+				if debug {
+					log.Println("transceiver: receiver exiting")
+				}
+				return
+			case n := <-rch:
+				for i := 0; i < n; i++ {
+					select {
+					case <-ctx.Done():
+						if debug {
+							log.Println("transceiver: receiver exiting")
+						}
+						return
+					case dst <- buff[i]:
+						//					fmt.Println(buff[i])
+					}
 				}
 			}
 		}
